0b0c123123f7e45caaa691570634afd17dd09a94
eliarbogast <arbogaste@carleton.edu>
2021-03-31 21:54:55 -0500

    Update Evolution.java

--- src/main/java/techperson1/evolution/mod/Evolution.java
+++ src/main/java/techperson1/evolution/mod/Evolution.java
@@ -3,6 +3,9 @@ package techperson1.evolution.mod;
 import net.fabricmc.api.ModInitializer;
 
 public class Evolution implements ModInitializer {
+
+    public static final String MOD_ID = "evolution";
+
     @Override
     public void onInitialize() {
 

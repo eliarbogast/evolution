d255549bc9411f1d94884ba749ab91938e3f1ad8
eliarbogast <arbogaste@carleton.edu>
2021-04-06 17:39:26 -0500

    Successfully added color changing functionality

---
+++ src/main/java/eliarbogast/evolution/mod/mixins/ColorTickMixin.java
@@ -0,0 +1,29 @@
+package eliarbogast.evolution.mod.mixins;
+
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.util.DyeColor;
+import org.lwjgl.system.CallbackI;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(SheepEntity.class)
+public abstract class ColorTickMixin {
+
+    @Shadow public abstract void setColor(DyeColor color);
+
+    @Inject(method="mobTick", at = @At("HEAD"))
+
+        public void mobTick(CallbackInfo info){
+        DyeColor[] colors = {DyeColor.GREEN, DyeColor.BLUE, DyeColor.MAGENTA};
+        Random random = new Random();
+        int rand = random.nextInt(3);
+        setColor(colors[rand]);
+        //setColor(DyeColor.BLUE);
+    }
+
+}
